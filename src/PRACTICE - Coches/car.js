ef3a89abda39608d319fc4562d452b72e8ef00b0
alumneifc33b <agarcia15183@alumnes.iesmanacor.cat>
2021-10-15 11:23:05 +0200

    19 to 25

--- src/PRACTICE - Coches/car.js
+++ src/PRACTICE - Coches/car.js
@@ -1,13 +1,26 @@
 class Car {
-    constructor(enrollment, model, color, price) {
+    constructor(enrollment, model, price, color) {
         this.enrollment = enrollment;
         this.model = model;
-        this.color = color;
         this.price = price;
+        this.color = color;
         this.type = "€";
     }
 
     info() {
-        return this.enrollment+", "+this.model+", "+this.color+", "+this.price+this.type;
+        return this.enrollment+", "+this.model+", "+this.price+this.type+", "+this.color;
+    }
+
+    convertToDollars(){
+        if (this.type==="€") {
+            this.price *= 1.16;
+            this.type = "$";
+        }
+    }
+    convertToEuros(){
+        if (this.type==="$") {
+            this.price /= 1.16;
+            this.type = "€";
+        }
     }
 }
