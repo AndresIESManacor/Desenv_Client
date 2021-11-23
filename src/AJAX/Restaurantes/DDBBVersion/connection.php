443a835fa1e36b73faf19da1cf1bd8a93b90f6ce
Andres Garcia Bauza <militaxx5@gmail.com>
2021-11-23 16:08:11 +0100

    started

--- src/AJAX/Restaurantes/DDBBVersion/connection.php
+++ src/AJAX/Restaurantes/DDBBVersion/connection.php
@@ -1,1 +1,12 @@
 <?php
+$servername = "localhost";
+$user = "root";
+$password = "militaxx5AGB5";
+$database = "restarante";
+
+$conn = new mysqli($servername, $user, $password, $database);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
--- src/AJAX/Restaurantes/DDBBVersion/index.php
+++ src/AJAX/Restaurantes/DDBBVersion/index.php
@@ -1,1 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <style>
+        body {
+            width: 100%;
+            height: 100vh;
+            text-align: center;
+        }
+        table, tr, th, td {
+            font-size: 32px;
+            border: skyblue solid 2px;
+        }
+        div {
+            text-align: center;
+            margin-left: 25%;
+        }
+    </style>
+</head>
+<body onload="loadDoc()">
+<h1>TABLE WITH XML</h1>
+<div>
+    <table id="table"></table>
+</div>
 <?php
+include("connection.php");
+$table = "restaurantes";
+
+$sql = "SELECT * FROM $table";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while ($row = $result->fetch_assoc()) {
+        $text .= $row["Id"] ." ". $row["Nom"] ." ".$row["Poblacio"]." ".$row["CategoriaPrecio"];
+    }
+}
+?>
+</body>
+</html>
