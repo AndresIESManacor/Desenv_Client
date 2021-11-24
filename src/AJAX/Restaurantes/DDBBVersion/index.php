0683477c84b29ff639f69e2307f1d13471d8181a
Andres Garcia Bauza <militaxx5@gmail.com>
2021-11-24 19:08:35 +0100

    DDBB Version terminado

--- src/AJAX/Restaurantes/DDBBVersion/index.php
+++ src/AJAX/Restaurantes/DDBBVersion/index.php
@@ -20,34 +20,54 @@
     </style>
 </head>
 <body onload="loadDoc()">
-<h1>TABLE WITH XML</h1>
+    <h1>TABLE WITH (PHP connected to DDBB)</h1>
 <div>
-    <table id="table"></table>
+    <table id="table">
+        <?php
+        $servername = "localhost";
+        $user = "root";
+        $password = "militaxx5AGB5";
+        $database = "restaurante";
+
+        $conn = new mysqli($servername, $user, $password, $database);
+
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        $table = "restaurante";
+
+        $sql = "SELECT * FROM $table";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            $text = "<tr><th>ID</th><th>NOMBRE</th><th>POBLACION</th><th>TIPO</th><th>CATEGORIA</th></tr>";
+            // output data of each row
+            while ($row = $result->fetch_assoc()) {
+                $text .= "<tr>";
+                $text .=
+                    "<td>". $row["Id"] ."</td>".
+                    "<td>". $row["Nom"] ."</td>".
+                    "<td>". $row["Poblacio"] ."</td>".
+                    "<td>". $row["Tipo"] ."</td>";
+
+                $categoria = $row["CategoriaPrecio"];
+                switch ($categoria) {
+                    case 1:
+                        $text .= "<td>Muy Barato!</td>";
+                        break;
+                    case 2:
+                        $text .= "<td>Barato!!</td>";
+                        break;
+                    case 3:
+                        $text .= "<td>Caro!!!</td>";
+                        break;
+                }
+                $text .= "</tr>";
+            }
+            echo $text;
+        }
+        ?>
+    </table>
 </div>
-<?php
-$servername = "localhost";
-$user = "root";
-$password = "militaxx5AGB5";
-$database = "restaurante";
-
-$conn = new mysqli($servername, $user, $password, $database);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-$table = "restaurantes";
-
-$sql = "SELECT * FROM $table";
-echo $sql;
-/*$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    // output data of each row
-    while ($row = $result->fetch_assoc()) {
-        $text .= $row["Id"] ." ". $row["Nom"] ." ".$row["Poblacio"]." ".$row["CategoriaPrecio"];
-    }
-}
-*/
-?>
 </body>
 </html>
