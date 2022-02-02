2aac1b26baaef1537a45b8b34023bb4d01e4d556
Andres Garcia Bauza <agarcia15183@alumnes.iesmanacor.cat>
2022-02-02 10:31:08 +0100

    axios y json

---
+++ src/REACT JS/jsonfetch/src/App.js
@@ -0,0 +1,46 @@
+import logo from './logo.svg';
+import './App.css';
+import {Component} from "react";
+
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      data:[]
+    }
+  }
+
+    componentDidMount() {
+        fetch('http://jsonplaceholder.typicode.com/posts')
+            .then(response => response.json())
+            .then(data => this.setState({ data }));
+    }
+
+  render() {
+    return (
+        <div className="App">
+          <div className="container">
+            <h1>Table JSON React </h1>
+            <table  className="table table-striped table-bordered">
+              <thead>
+              <tr><th>Id</th><th>UserId</th><th>Title</th><th>Body</th></tr>
+              </thead>
+              <tbody>{this.state.data.map(function(item, key) {
+                return (
+                    <tr key = {key}>
+                      <td>{item.id}</td>
+                      <td>{item.userId}</td>
+                      <td>{item.title}</td>
+                      <td>{item.body}</td>
+                    </tr>
+                )
+              })}</tbody>
+            </table>
+          </div>
+        </div>
+    )
+  }
+}
+
+export default App;
